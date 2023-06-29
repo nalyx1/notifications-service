59962e2f7d036844d96ec540152e8f912419c7d7
João Rodrigues <joaorodriguez0106@gmail.com>
2023-06-29 16:21:32 -0300

    feat: notification crud

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, Delete, Param, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UpdateNotificationDto } from './dto/update-notification.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  findAll() {
+    return this.appService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.appService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createNotificationDto: CreateNotificationDto) {
+    return this.appService.create(createNotificationDto)
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateNotificationDto: UpdateNotificationDto,
+  ) {
+    return this.appService.update(id, updateNotificationDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.appService.remove(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,42 @@
+import { CreateNotificationDto } from './dto/create-notification.dto';
+import { UpdateNotificationDto } from './dto/update-notification.dto';
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prismaService: PrismaService) {}
+
+  findAll() {
+    return this.prismaService.notification.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.notification.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    });
+  }
+
+  create(createNotificationDto: CreateNotificationDto) {
+    return this.prismaService.notification.create({
+      data: createNotificationDto,
+    });
+  }
+
+  update(id: string, updateNotificationDto: UpdateNotificationDto) {
+    return this.prismaService.notification.update({
+      where: { id },
+      data: updateNotificationDto,
+    });
+  }
+
+  remove(id: string) {
+    return this.prismaService.notification.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
