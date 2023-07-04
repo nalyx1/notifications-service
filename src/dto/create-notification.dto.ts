9dc69f1fb222f3c39eb3f05ae8377e48f63c8907
João Rodrigues <joaorodriguez0106@gmail.com>
2023-07-03 22:09:21 -0300

    feat: class-validator decorators

--- src/dto/create-notification.dto.ts
+++ src/dto/create-notification.dto.ts
@@ -1,5 +1,14 @@
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+
 export class CreateNotificationDto {
+  @IsNotEmpty()
+  @IsUUID()
   recipient_id: string;
+
+  @IsNotEmpty()
+  @Length(5, 256)
   content: string;
+
+  @IsNotEmpty()
   category: string;
 }
